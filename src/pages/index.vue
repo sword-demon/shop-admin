b3717d80f2dbcf15bd7b54b72307f480c3aa7ee3
wxvirus <1418667580@qq.com>
2022-11-28 00:49:43 +0800

    index page v-permission 调整

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <el-row :gutter="20" v-permission="['getStatistic1,GET']">
+        <el-row :gutter="20" v-permission="['getStatistics1,GET']">
             <template v-if="panels.length == 0">
                 <el-col :span="6" v-for="i in 4" :key="i">
                     <el-skeleton style="width: 100%" animated loading>
@@ -9,41 +9,41 @@
                                 <template #header>
                                     <div class="flex justify-between">
                                         <el-skeleton-item
-                                            variant="p"
-                                            style="width: 50%"
+                                                variant="p"
+                                                style="width: 50%"
                                         />
                                         <el-skeleton-item
-                                            variant="p"
-                                            style="width: 10%"
+                                                variant="p"
+                                                style="width: 10%"
                                         />
                                     </div>
                                 </template>
                                 <el-skeleton-item
-                                    variant="h3"
-                                    style="width: 80%"
+                                        variant="h3"
+                                        style="width: 80%"
                                 />
-                                <el-divider />
+                                <el-divider/>
                                 <div
-                                    class="
+                                        class="
                                         flex
                                         justify-between
                                         text-sm text-gray-500
                                     "
                                 >
                                     <el-skeleton-item
-                                        variant="p"
-                                        style="width: 50%"
+                                            variant="p"
+                                            style="width: 50%"
                                     />
                                     <el-skeleton-item
-                                        variant="p"
-                                        style="width: 10%"
+                                            variant="p"
+                                            style="width: 10%"
                                     />
                                 </div>
                             </el-card>
                             <div style="padding: 14px">
                                 <el-skeleton-item
-                                    variant="p"
-                                    style="width: 50%"
+                                        variant="p"
+                                        style="width: 50%"
                                 />
                             </div>
                         </template>
@@ -51,25 +51,26 @@
                 </el-col>
             </template>
             <el-col
-                :span="6"
-                :offset="0"
-                v-for="(item, index) in panels"
-                :key="index"
+                    :span="6"
+                    :offset="0"
+                    v-for="(item, index) in panels"
+                    :key="index"
             >
                 <el-card shadow="hover" class="border-0">
                     <template #header>
                         <div class="flex justify-between">
                             <span class="text-sm">{{ item.title }}</span>
                             <el-tag :type="item.unitColor" effect="plain">{{
-                                item.unit
-                            }}</el-tag>
+                                    item.unit
+                                }}
+                            </el-tag>
                         </div>
                     </template>
                     <span class="text-3xl font-bold text-gray-500">
                         {{ item.value }}
-                        <CountToVue :value="item.value" />
+                        <CountToVue :value="item.value"/>
                     </span>
-                    <el-divider />
+                    <el-divider/>
                     <div class="flex justify-between text-sm text-gray-500">
                         <span>{{ item.subTitle }}</span>
                         <span>{{ item.subValue }}</span>
@@ -78,23 +79,23 @@
             </el-col>
         </el-row>
 
-        <IndexNavs />
+        <IndexNavs/>
 
         <el-row :gutter="20" class="mt-5">
             <el-col :span="12" :offset="0">
-                <IndexChart v-permission="['getStatistics3,GET']" />
+                <IndexChart v-permission="['getStatistics3,GET']"/>
             </el-col>
-            <el-col :span="12" :offset="0" v-permission="['getStatistic1,GET']">
+            <el-col :span="12" :offset="0" v-permission="['getStatistics1,GET']">
                 <IndexCard
-                    title="店铺及商品提示"
-                    tip="店铺及商品提示"
-                    :btns="goods"
-                    class="mb-3"
+                        title="店铺及商品提示"
+                        tip="店铺及商品提示"
+                        :btns="goods"
+                        class="mb-3"
                 />
                 <IndexCard
-                    title="交易提示"
-                    tip="需要立即处理的交易订单"
-                    :btns="order"
+                        title="交易提示"
+                        tip="需要立即处理的交易订单"
+                        :btns="order"
                 />
             </el-col>
         </el-row>
@@ -102,8 +103,8 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { getStatistics1, getStatistics2 } from "~/api/index.js";
+import {ref} from "vue";
+import {getStatistics1, getStatistics2} from "~/api/index.js";
 import CountToVue from "~/components/CountTo.vue";
 import IndexNavs from "~/components/IndexNavs.vue";
 import IndexChart from "~/components/IndexChart.vue";
